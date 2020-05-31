40e31ab65c63d9d1d92a97103ec9e198c6d4436d
Riaz Uddin <riaz.uddin@teradata.com>
2020-05-30 22:41:37 -0700

    spring boot application

---
+++ spring-boot-rest/README.md
@@ -0,0 +1,11 @@
+### Spring application
+
+Build application: ./gradlew build
+
+Start application:
+ 
+    ./gradlew bootRun
+    or
+    java -jar build/libs/spring-boot-rest-0.0.1-SNAPSHOT.jar
+
+Application will be running at port 9999: http://localhost:9999/api/rooms
---
+++ spring-boot-rest/src/main/java/com/example/spring/boot/rest/springbootdata/RoomServices.java
@@ -0,0 +1,23 @@
+package com.example.spring.boot.rest.springbootdata;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class RoomServices {
+
+    private static List<Room> rooms = new ArrayList<>();
+
+    static {
+        for (int i = 0; i < 10; i++) {
+            rooms.add(new Room(i, "Rooom"+i, "R"+i, "Q"));
+        }
+    }
+
+
+    public List<Room> getAllRooms() {
+        return rooms;
+    }
+}
